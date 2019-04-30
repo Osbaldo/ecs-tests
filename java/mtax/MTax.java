60ce69542fd7a20c4ff1eb4f5f18ec8e5a5262de
Morter <osbaldo_cl@hotmail.com>
2019-04-30 18:26:35 -0500

    Add files via upload
    Style[MTax]: Se eliminaron las líneas comentadas para una mejor comprensión.
    Chore[MTax]: Se agrego un comentario al metodo 'Mtax()' ya que no contiene nada y se agregó un comentario con poca descripción.
    Refactor[MTax]: Se cambió el metodo de validación, ahora solo pasa por un 'isEmpty()' en lugar de 2 tipos de validaciones
    Style[MTax]: Se cambió el nombre de la variable 'map_taxs' a 'mapTaxs'para que sea mas facil de leer
    Refactor[MTax]: Se cambió la forma en que se inicializa el mapTaxs, usaba una forma ya depreciada de inicizalizar la lista.
    Docs[MTax]: Se agregan comentarios descriptivos.
    Chore[MTax]: Se eliminó la validación de 'isLocal()' para optimización de procesamiento y comprensión.
    
    
    DUDA: Se llama al método 'TaxsByListId()' pero no esta definido, ¿se supone que yo lo debía hacer viendo que es lo que se utiliza de la variable mas adelante?
    NOTA: tenía la intención de hacer pruebas unitarias pero no con ese metodo incompleto.

--- java/mtax/MTax.java
+++ java/mtax/MTax.java
@@ -2,77 +2,59 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 
-public class MTax implements Constant {
+public class MTax {
     
-    public MTax(){
-        
+    public void mTax() {
+    	// Do something
     }
-    
+	
     public static List<String> validate(List<X_Tax> xTaxList) {
         
         List<String> errorList = new ArrayList<>();
         
-//        List<String> taxCategoryList = MInfoTaxCategory.getTaxCategoryStringList();
-        
-        if(xTaxList != null && xTaxList.size() > 0) {
+        // checks is xTaxList is empty
+        if(!xTaxList.isEmpty()) {
+        	// Initialize validIds
             List<String> validIds = new ArrayList<>();
             int cont = 0;
             for (X_Tax tax : xTaxList) {
+            	// checks for ID existence
                 if(tax.getId() != null){
                     validIds.add(tax.getId().toString());
-                }
-//                if(tax.getAmount() == null) {
-//                    errorList.add("El importe es obligatorio");
-//                }
-
-                if(tax.getTax() == null) {
-                    errorList.add("El impuesto es obligatorio");
-                }
-//                else if(!taxCategoryList.contains(tax.getTax())) {
-//                    errorList.add("El impuesto no es un dato valido");
-//                }
-
-//                if(tax.isLocal()){
-//                    if(tax.isTrasladado() && tax.getTaxAmount() == null ) {
-//                        errorList.add("El importe es obligatorio");
-//                    }
-//                } 
-//                else {
-//                    if(tax.getTaxAmount() == null ) {
-//                        errorList.add("El importe es obligatorio");
-//                    }
-//                }
-                
-                if(!tax.isLocal()){
-                    cont++;
-                }
-            }
-            if(cont<=0){
-                errorList.add("Debe de incluir al menos una tasa no local");
-            }
-            if(validIds.size() > 0){
+                 // checks for Tax
+                    if(tax.getTax() == null) {
+                        errorList.add("El impuesto es obligatorio");
+                    }
+                    if(!tax.isLocal()){
+                    	//checks for non-local taxes
+                        cont++;
+                    }
                     
+                }
+                
+                
+
+            }
+            if(cont<=0) errorList.add("Debe de incluir al menos una tasa no local");
+            
+            if(!validIds.isEmpty()){
+                    // checks if validIds is empty
                     List<X_Tax> xt = TaxsByListId(validIds, false);
                     if(xt.size() != validIds.size()){
                         errorList.add("Existen datos no guardados previamente");
                     }else{
-                        HashMap<String, X_Tax> map_taxs = new HashMap<String, X_Tax>();
+                    	HashMap<String, X_Tax> mapTaxs = new HashMap<>();
                         for(X_Tax tax: xt){
-                            map_taxs.put(tax.getId().toString(), tax);
+                            mapTaxs.put(tax.getId().toString(), tax);
                         }
                         for(int i = 0; i < xTaxList.size(); i++){
                             if(xTaxList.get(i).getId() != null){
-                                xTaxList.get(i).setCreated(
-                                        map_taxs.get(xTaxList.get(i).getId().toString())
-                                                .getCreated());
+                                xTaxList.get(i).setCreated(mapTaxs.get(xTaxList.get(i).getId().toString()).getCreated());
                             }
                         }
                     }
             }
         }
-//        else {
-//            errorList.add("El documento no tiene tasas");
-//        }
         
         return errorList;
     }
---
+++ java/mtax/MTaxTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MTaxTest {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@Test
+	void test() {
+		//fail("Not yet implemented");
+		logger.log("error");
+	}
+
+}
